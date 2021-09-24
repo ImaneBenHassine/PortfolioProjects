6005119aa71ae057a6fdff0670d8d43818bfa3e1
Imane <imen.bh17@gmail.com>
2021-09-24 18:32:06 +0200

    Add files via upload

---
+++ SQL Query for Tableau.sql
@@ -0,0 +1,38 @@
+--In this project we will be taking some of the SQL queries we worked on in the first project and creating visualizations in Tableau.
+-- but we can't connect Tableau to SQL so SQL-->Ecxel-->Tableau
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From PortfolioProjects..CovidDeaths
+where continent is not null 
+order by 1,2
+
+--We take these out as they are not inluded in the above queries and want to stay consistent
+-- European Union is part of Europe
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From PortfolioProjects..CovidDeaths
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProjects..CovidDeaths
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProjects..CovidDeaths
+Group by Location, Population, date
+order by PercentPopulationInfected desc
+
+Select dea.continent, dea.location, dea.date, dea.population
+, MAX(vac.total_vaccinations) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From PortfolioProjects..CovidDeaths dea
+Join PortfolioProjects..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+group by dea.continent, dea.location, dea.date, dea.population
+order by 1,2,3
